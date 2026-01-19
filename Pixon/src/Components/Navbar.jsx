7541265ca5407ca9fb9627ca9de9f0d94981d6ec
kanchan kahar <kaharkanchan884@gmail.com>
2026-01-19 22:19:37 +0530

    added

--- Pixon/src/Components/Navbar.jsx
+++ Pixon/src/Components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import logo from '../assets/logo.svg'
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='w-full h-24  flex justify-evenly items-center '>
+            {/* logo */}
+            <div className='text-center'>
+                <img className='text-center p-5' src={logo} alt="" />
+            </div>
+            {/* Navbar */}
+            <div className='flex text-xl font-bold gap-10 list-none text-white px-12  py-5 rounded-4xl bg-[#FFFFFF0F]'>
+                <li className='decoration-0'>Home</li>
+                <li>About Us</li>
+                <li>Servics</li>
+                <li>Blog</li>
+                <li>Pages</li>
+                <li>Contact </li>
+
+            </div>
+            {/* Menu */}
+            <div>
+                <button className=' bg-gradient-to-r from-[#6BFDD9] to-[#F0FF6C] rounded-4xl font-semibold px-5 py-4'>DOWNLOAD CV</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
